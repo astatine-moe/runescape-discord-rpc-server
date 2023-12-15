35eff285da02b9df1d5d92d83feec61ed3e27b39
Morgan Evans <morgan@morganevans.dev>
2023-12-15 16:57:05 +0000

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,9 @@
+namespace runescape_discord_rpc_server;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+namespace runescape_discord_rpc_server;
+
+static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        // To customize application configuration such as set high DPI settings or default font,
+        // see https://aka.ms/applicationconfiguration.
+        ApplicationConfiguration.Initialize();
+        Application.Run(new Form1());
+    }    
+}
