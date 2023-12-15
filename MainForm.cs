11f0ebfc85e1a4cd01062d4201c46d79c2595e87
Morgan Evans <morgan@morganevans.dev>
2023-12-15 18:18:31 +0000

    Tray item management

--- MainForm.cs
+++ MainForm.cs
@@ -14,13 +14,33 @@ namespace runescape_discord_rpc_server
     {
 
         private ContextMenuStrip contextMenuStrip;
+        private ToolStripMenuItem startMenuItem;
+        private ToolStripMenuItem stopMenuItem;
+        private ToolStripSeparator toolStripSeparator;
+        private ToolStripLabel statusLabel;
+        private string applicationState = "Stopped";
         public MainForm()
         {
             InitializeComponent();
 
             contextMenuStrip = new ContextMenuStrip();
-            contextMenuStrip.Items.Add("Start", null, StartAction);
-            contextMenuStrip.Items.Add("Stop", null, StopAction);
+
+            startMenuItem = new ToolStripMenuItem($"Start", null, StartAction);
+            stopMenuItem = new ToolStripMenuItem($"Stop", null, StopAction);
+            statusLabel = new ToolStripLabel($"RPC Status: {applicationState}");
+
+            // Create a horizontal line divider
+            toolStripSeparator = new ToolStripSeparator();
+
+            // Set initial visibility
+            startMenuItem.Visible = true;
+            stopMenuItem.Visible = false;
+
+
+            contextMenuStrip.Items.Add(statusLabel);
+            contextMenuStrip.Items.Add(toolStripSeparator);
+            contextMenuStrip.Items.Add(startMenuItem);
+            contextMenuStrip.Items.Add(stopMenuItem);
             contextMenuStrip.Items.Add("Exit", null, ExitAction);
 
             notifyIcon1.ContextMenuStrip = contextMenuStrip;
@@ -31,12 +51,21 @@ namespace runescape_discord_rpc_server
         private void StartAction(object sender, EventArgs e)
         {
             // Implement the "Start" action
-            // Show your form or perform necessary actions
+            // Update state and menu items
+            applicationState = "Running";
+            startMenuItem.Visible = false; // Hide "Start"
+            stopMenuItem.Visible = true;   // Show "Stop"
+            UpdateStatusLabel();
         }
 
         private void StopAction(object sender, EventArgs e)
         {
             // Implement the "Stop" action
+            // Update state and menu items
+            applicationState = "Stopped";
+            startMenuItem.Visible = true;    // Show "Start"
+            stopMenuItem.Visible = false;    // Hide "Stop"
+            UpdateStatusLabel();
         }
 
         private void ExitAction(object sender, EventArgs e)
@@ -48,6 +77,11 @@ namespace runescape_discord_rpc_server
         private void MainForm_Load(object sender, EventArgs e)
         {
             this.Hide();
+        }
+        private void UpdateStatusLabel()
+        {
+            // Update the status label text
+            statusLabel.Text = $"RPC Status: {applicationState}";
         }
     }
 }
